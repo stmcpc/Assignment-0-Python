66539fddd7fc34b8d9ddb538e68ab0a221faf24c
Dormey <dromey13@gmail.com>
2021-02-20 00:47:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from solutions import evenOrOdd
+evenOrOdd()
+#Your first assignment deals with numbers.
+#Check and display whether each number in a given list of numbers is even or odd.
+
+#The code below does not work, work together to fix that.
+#Click run to test your progress.
+
+print("\nYOUR PROGRAM:")
+list = [6, 9, 4, 2, 0, 1, 3, 3, 7]
+for num in cyst:
+  if mun > 5:
+    print("{} is even".format(num))
+
---
+++ solutions.py
@@ -0,0 +1,10 @@
+
+def evenOrOdd():
+  list = [6, 9, 4, 2, 0, 1, 3, 3, 7]
+  print("YOUR PROGRAM SHOULD MATCH THIS:")
+  for num in list:
+    if num %2 == 0:
+      print("{} is even".format(num))
+    else:
+      print("{} is odd".format(num))
+
